ff126221d1b8f819645f9c0f5c02544c471e0ea3
Daniel Cohen <dcohen@mobiquityinc.com>
2013-09-13 01:57:00 -0400

    changing things

--- hello.js
+++ hello.js
@@ -2,3 +2,5 @@ console.log("Hello Ken!");
 console.log("How is life?!");
 console.log("Hope it is well!");
 console.log("Surely you must be kidding");
+console.log("Hello Daniel!");
+console.log("Good to see you!");
